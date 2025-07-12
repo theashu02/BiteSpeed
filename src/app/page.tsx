3b52479aa02646291ec7f87e73c5923e3d5aebf5
theashu02 <ashutoshchauhan1919@gmail.com>
2025-07-13 01:32:40 +0530

    react flow added properly and working with bun

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,10 @@
 import { Button } from "@/components/ui/button";
+import Flow from "./components/Flow";
 
 export default function Home() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
-      Hello
+      <Flow />
       <Button variant="outline" className="p-5 bg-amber-200">Hello button</Button>
     </div>
   );
